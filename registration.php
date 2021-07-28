4c30f4a08c70dbdfd0bea35727ee6005b0e1cf6f
Adrian Romero Vargas <ak360s@gmail.com>
2021-07-28 10:05:32 -0500

    Update registration.php

--- registration.php
+++ registration.php
@@ -5,6 +5,6 @@
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'AdrianRoma_Ivoy',
+    'Adrianroma_Ivoy',
     __DIR__
 );
